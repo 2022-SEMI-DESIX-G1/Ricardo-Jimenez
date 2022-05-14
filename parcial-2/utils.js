9800652726e7f33491583e1049a426637459935a
Alexander Jimenez <alexjr1422@Alexanders-MacBook-Pro.local>
2022-05-13 23:19:00 -0500

    Finalizacion de JS

--- parcial-2/utils.js
+++ parcial-2/utils.js
@@ -1,10 +1,10 @@
 (() => {
     const Utils = {
       settings: {
-        backendBaseUrl: "https://pokeapi.co/api/v2",
+        urlBase: "https://pokeapi.co/api/v2",
       },
       getFormattedBackendUrl: ({ query, searchType }) => {
-        return `${Utils.settings.backendBaseUrl}/${searchType}/${query}`;
+        return `${Utils.settings.urlBase}/${searchType}/${query}`;
       },
       getPokemon: ({ query, searchType = "pokemon" }) => {
         return Utils.fetch({
@@ -12,14 +12,18 @@
           searchType,
         });
       },
+      getEvolution:  (url)=>{
+        return Utils.fetch({url, searchType: ''})
+      },
       fetch: async ({ url, searchType }) => {
         try {
           const rawResponse = await fetch(url);
-          if (rawResponse.status !== 200) {
+          if (rawResponse.status !== 200) {   
             throw new Error(`${searchType} not found`);
           }
           return rawResponse.json();
         } catch (error) {
+          
           throw error;
         }
       },
